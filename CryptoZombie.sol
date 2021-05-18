6b6eed1e78e8fd91c66efc9dc55823653a84c9ea
apoorva <apoorvasrivastava.14@gmail.com>
2021-05-18 13:16:23 +0530

    updated cryptozombie

--- CryptoZombie.sol
+++ CryptoZombie.sol
@@ -1,35 +1,80 @@
-pragma solidity >=0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
 
 contract ZombieFactory {
-    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-    uint256 dnaDigits = 16;
-    uint256 dnaModulus = 10**dnaDigits;
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 
     struct Zombie {
         string name;
-        uint256 dna;
+        uint dna;
     }
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint256 _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
         zombies.push(Zombie(_name, _dna));
-        uint256 id = zombies.length - 1;
+        uint id = zombies.length-1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string memory _str)
-        private
-        view
-        returns (uint256)
-    {
-        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
-        uint256 randDna = _generateRandomDna(_name);
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
+
 }
+
+interface KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory {
+
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))){
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);    
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
+}
+
